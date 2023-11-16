3e719b0a11c0cb5a8c0948e2b6aa4badfd887a2e
David Jeshua Áreas Pérez <142626297+David7J@users.noreply.github.com>
2023-11-15 21:23:36 -0600

    Add files via upload

---
+++ Ejercicio3/Asiento.java
@@ -0,0 +1,26 @@
+class Asiento {
+    private String etiqueta;
+    private boolean ocupado;
+
+    public Asiento(String etiqueta) {
+        this.etiqueta = etiqueta;
+        this.ocupado = false;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public void ocuparAsiento() {
+        this.ocupado = true;
+    }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
+
+    @Override
+    public String toString() {
+        return etiqueta + (ocupado ? " (Ocupado)" : " (Libre)");
+    }
+}
